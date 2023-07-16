21617a7c3b27d6bd73d89f6ce3e6cc872bc98327
Ayush <ayushking.0606@gmail.com>
2023-07-16 12:04:26 +0530

    first commitby me

---
+++ practice.js
@@ -0,0 +1,194 @@
+
+
+// function unique_char(str1)
+// {
+//  var str=str1;
+//  var uniql="";
+//  for (var x=0;x < str.length;x++)
+//  {
+
+//  if(uniql.indexOf(str.charAt(x))==-1)
+//   {
+//   uniql += str[x];  
+  
+//    }
+//   }
+//   return uniql;  
+// }  
+// console.log(unique_char("thequickbrownfoxjumpsoverthelazydog"));
+
+
+
+
+
+
+// ----------------------------------------------------------------------------------
+// ============================================================================
+// _______________________________________________________________________________
+
+
+//  Date 01-july-2023
+
+
+// setInterval(function,time in mile second)
+// setInterval(function(),3000)
+
+
+// Q 1----------------?
+
+// function sing(){
+//     console.log("sing a song")
+// }
+
+// setInterval(sing,3000);
+// clearInterval(sing,12000);
+// __________________________________________________________________
+
+// Let students={ “molik” ,” ayush”, “shivam”, “shivani”, “prince”}
+
+// student[1]=”amit”;
+// student[5]=”riyaz”;
+// console.log(student);
+
+
+// .........Empty Array............
+
+// var student=[];
+// var student= newArray();
+
+// --->  length= student.length
+// --->  Pop()method :--> remove last elements from the Array
+
+// ---------Example--------------
+
+// var number=[1,3,2,4,5,7,6];
+
+
+// console.log(number.pop());
+// console.log(number);
+
+// ------------Push()method :->
+//  console.log(number.push(8));
+//  console.log(number);
+
+//  unshift
+// console.log(number.unshift(0));
+// console.log(number);
+
+// ------------shift method :->
+// console.log(number.shift());
+// console.log(number);
+
+
+
+
+// ----------indexof method:->
+// console.log(number.indexOf(6));
+// console.log(number.indexOf(9));
+
+// *******IF the value is not available in the array the output will => -1 ***********
+
+// --------slice() method:->
+// var num1= number.slice(1,3);
+// console.log(num1);
+
+// ---------toString() method:-->
+
+// number.toString();
+
+
+// // ------join() method:-->
+// number.join("*");
+// 1*2*3*4*5*6*7
+
+// ------delete() method:-->
+// delete number[1];
+// console.log(number)
+
+
+
+// ------concat() method:-->
+// var Rollno= [30,46,39,59,]
+// var abs = number.concat(Rollno);
+// console.log(abs);
+
+
+
+// ------splice() method:-->add the element into the new array
+
+// number.splice(2,0,9)
+// console.log(number);
+// position, Element removed
+
+// ------sort() method:-->1,3,4,2 = 1,2,3,4
+// number.sort();
+// console.log(number);
+
+// ------reverse() method:-->3,2,1,0
+// number.reverse();
+// console.log(number);
+
+
+
+// function first(Array){
+//     console.log(Array[0])
+
+
+// }
+
+// first([1,2,3,4,5,6,7])
+
+
+
+
+// Date 08 july 2023_________________>>>>
+
+
+// var word = 458
+// console.log(typeof word);
+
+
+
+
+
+
+// var classroom ={
+
+//     name:"amit",
+//     age :26,
+//     rollno:3
+
+// }
+// // console.log("<br>")
+// console.log(classroom)
+// console.log(classroom.name, classroom.age)
+// console.log(classroom.name="Ayush")
+
+
+// classroom.section="B"; 
+
+// console.log(classroom)
+
+// delete classroom.name
+
+// console.log(classroom)
+
+// // classroom.name(pop())
+
+// Ayush={
+//     name:[1,2,3,4]
+// }
+// Ayush.name.pop()
+// console.log(Ayush.name.pop())
+// console.log(Ayush)
+
+
+// function hello(){
+//     alert("goodmorning")
+//     document.write("Hello Moto")
+//     console.log("Byy Molik")
+   
+
+// }
+
+// document.write([])<img src="https://thumbs.dreamstime.com/b/apple-logo-19106337.jpg" alt="">
